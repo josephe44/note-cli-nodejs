fd9b92588457e2a90765bb3956e9f9c562e64053
josephe44 <josephe442@gmail.com>
2023-07-25 16:14:34 +0100

    added server and display on the web

--- src/command.js
+++ src/command.js
@@ -7,6 +7,7 @@ import {
   removeNote,
   removeAllNotes,
 } from "./notes.js";
+import { start } from "./server.js";
 
 const listNote = (notes) => {
   notes.forEach(({ id, content, tags }) => {
@@ -87,7 +88,10 @@ yargs(hideBin(process.argv))
         type: "number",
       });
     },
-    async (argv) => {}
+    async (argv) => {
+      const notes = await getAllNote();
+      start(notes, argv.port);
+    }
   )
   .command(
     "clean",
